c1b4d2af07ef0ea719cdcdf06d512ef01490bd3f
D-Moskalyov <moscalevd@Gmail.com>
2015-10-11 21:03:17 +0300

    initial

---
+++ 14.04.06_explorer/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Cars_23._03._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<FileSystemInfo> fSI = new List<FileSystemInfo>();
+            string path;
+            int index = 0;
+            int lineCnt = 0;
+            ConsoleKeyInfo key_info = new ConsoleKeyInfo();
+            bool exist = false; 
+            DirectoryInfo di;
+            DirectoryInfo diCopy;
+            do
+            {
+                Console.WriteLine("Введите директорию");
+                path = Console.ReadLine();
+                di = new DirectoryInfo(path);
+                exist = di.Exists;
+            } while (!exist);
+            fSI.AddRange(di.GetFiles().ToList());
+            fSI.AddRange(di.GetDirectories().ToList());
+            PrintTree(index, fSI);
+            lineCnt = fSI.Count;
+            do
+            {
+                switch (key_info.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        {
+                            if (index > 0) index--;
+                            else index = lineCnt;
+                            PrintTree(index, fSI);
+                            break;
+                        }
+                    case ConsoleKey.DownArrow:
+                        {
+                            if (index < lineCnt) index++;
+                            else index = 0;
+                            PrintTree(index, fSI);
+                            break;
+                        }
+                    case ConsoleKey.Enter:
+                        {
+                            if (index == 0)
+                            {
+                                if (di.Parent != null)
+                                {
+                                    di = di.Parent;
+                                    fSI.Clear();
+                                    fSI.AddRange(di.GetFiles().ToList());
+                                    fSI.AddRange(di.GetDirectories().ToList());
+                                    lineCnt = fSI.Count;
+                                }
+                                else
+                                    break;
+                            }
+                            else
+                            {
+                                if ((fSI[index - 1].Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                                {
+                                    di = (DirectoryInfo)fSI[index - 1];
+                                    fSI.Clear();
+                                    fSI.AddRange(di.GetFiles().ToList());
+                                    fSI.AddRange(di.GetDirectories().ToList());
+                                    lineCnt = fSI.Count;
+                                }
+                                else
+                                    break;
+                            }
+                            index = 0;
+                            PrintTree(index, fSI);
+                            break;
+                        }
+                    case ConsoleKey.C:
+                        {
+                            if (index != 0)
+                            {
+                                if (!((fSI[index - 1].Attributes & FileAttributes.Directory) == FileAttributes.Directory))
+                                {
+                                    do
+                                    {
+                                        Console.WriteLine("Введите дирректорию");
+                                        path = Console.ReadLine();
+                                        diCopy = new DirectoryInfo(path);
+                                        exist = diCopy.Exists;
+                                        if (exist)
+                                        {
+                                            if ((diCopy.Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                                            {
+                                                ((FileInfo)fSI[index - 1]).CopyTo(diCopy.FullName + "\\" + fSI[index - 1].Name.ToString());
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine("Не верно. Отмена? (Y - отмена)");
+                                                key_info = Console.ReadKey(true);
+                                                if (key_info.Key == ConsoleKey.Y)
+                                                {
+                                                    break;
+                                                }
+                                            }
+                                        }
+                                        else
+                                        {
+                                            Console.WriteLine("Не верно. Отмена? (Y - отмена)");
+                                            key_info = Console.ReadKey(true);
+                                            if (key_info.Key == ConsoleKey.Y)
+                                            {
+                                                break;
+                                            }
+                                        }
+                                    } while (!exist);
+                                }
+                                //else index = 1;
+                                PrintTree(index, fSI);
+                                break;
+                            }
+                            break;
+                        }
+                    default:
+                        break;
+                }
+                key_info = Console.ReadKey(true);
+            } while (key_info.Key != ConsoleKey.Escape);
+        }
+        static void PrintTree(int indx, List<FileSystemInfo> fsi)
+        {
+            Console.Clear();
+            if (indx == 0)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.WriteLine("...");
+                Console.BackgroundColor = ConsoleColor.Black;
+            }
+            else
+                Console.WriteLine("...");
+            for (int i = 1; i <= fsi.Count; i++)
+            {
+                if (indx == i)
+                {
+                    if ((fsi[i - 1].Attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                    {
+                        Console.BackgroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine(fsi[i - 1].ToString());
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                    else
+                    {
+                        Console.BackgroundColor = ConsoleColor.Blue;
+                        Console.WriteLine(fsi[i - 1].ToString());
+                        Console.BackgroundColor = ConsoleColor.Black;
+                    }
+                }
+                else
+                    Console.WriteLine(fsi[i - 1].ToString());
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("c для копирования файлов");
+        }
+    }
+}
---
+++ 14.04.06_explorer/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("14.04.06_explorer")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("14.04.06_explorer")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("cc8db96b-fb98-4474-b387-ba23303a1f4e")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
